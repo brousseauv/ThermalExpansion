65721e0eacb11b58d834931901aa1e0606cb36cf
brousseauv <sorbidelimpide@gmail.com>
2019-06-10 15:49:47 -0400

    =started hexagonal symmetry for gruneisens

---
+++ elasticfile.py
@@ -0,0 +1,31 @@
+#! \usr/bin/env python
+
+from __future__ import print_function
+
+__author__ = "Veronique Brousseau-Couture"
+
+import os
+import warnings
+import netCDF4 as nc
+import numpy as np
+
+from constants import gpa_to_habo3
+from ElectronPhononCoupling import EpcFile
+
+class ElasticFile(EpcFile):
+
+    def read_nc(self, fname=None):
+
+        # Open the OUT.nc file and read it
+        fname = fname if fname else self.fname
+
+        super(ElasticFile, self).read_nc(fname)
+
+        with nc.Dataset(fname, 'r') as root:
+
+            self.stiffness_relaxed = root.variables['elastic_constants_relaxed_ion'][:,:]/gpa_to_habo3
+            self.stiffness_clamped = root.variables['elastic_constants_clamped_ion'][:,:]/gpa_to_habo3
+            self.compliance_relaxed = root.variables['elastic_constants_relaxed_ion'][:,:]/gpa_to_habo3
+            self.compliance_clamped = root.variables['elastic_constants_clamped_ion'][:,:]/gpa_to_habo3
+
+        
