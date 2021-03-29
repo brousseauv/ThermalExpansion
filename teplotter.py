75ac5d8721f17f3487ae786276e9157e33e97db5
brousseauv <veronique.brousseauc@gmail.com>
2021-03-29 11:26:54 -0400

    Revert "minor fixes"
    This reverts commit 6f4284b37b1ca419a7043bbb942a4cbcb697d644.

--- teplotter.py
+++ teplotter.py
@@ -7,12 +7,11 @@ __author__ = "brousseauv"
 from .plotter import Plotter
 from .tefile import set_file_class
 from expfile import EXPfile
-from constants import bohr_to_ang, j_to_ev, avogadro
+from constants import bohr_to_ang
 from matplotlib import rc
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 from matplotlib.lines import Line2D
-import numpy as np
 
 rc('text', usetex=True)
 rc('font', family='sans-serif', weight='bold')
@@ -63,24 +62,17 @@ class TEplot(Plotter):
                 else:
                     self.ax.plot(ncfile.xaxis, ncfile.yaxis, marker='x', **kwargs)
 
-    def set_yref(self, f, x, val, style='solid'):
-
-        # Plot a horizontal line at y = val
-        zer = val*np.ones_like(x)
-        f.plot(x, zer, color='black', linestyle=style)
-
 
 class AcellPlot(TEplot):
     """
     Plots temperature dependent lattice parameters
     """
 
-    def __init__(self, ax=None, field='acell', shift_acell=None, **kwargs):
+    def __init__(self, ax=None, field='acell', **kwargs):
         self.field = field
-        self.shift_acell = shift_acell
         super(AcellPlot, self).__init__(ax=ax)
 
-    def plot_te(self, te_data, marker_list=None, color_list=None, shift_acell=None, **kwargs):
+    def plot_te(self, te_data, marker_list=None, color_list=None, **kwargs):
 
         if self.field == 'acell' or self.field == 'acell_a':
             a = 0
@@ -100,15 +92,6 @@ class AcellPlot(TEplot):
                 else:
                     self.ax.plot(ncfile.temperature, ncfile.acell[a, :], **kwargs)
 
-            # Add a dashed line shifting the numerical result closer to experimental data
-            if self.shift_acell is not None:
-                shift = (ncfile.acell[a, 0] - self.shift_acell[a])*np.ones((ncfile.ntemp))
-                if color_list:
-                    self.ax.plot(ncfile.temperature, ncfile.acell[a, :]-shift, color=color_list[i],
-                                 dashes=[2, 2], **kwargs)
-                else:
-                    self.ax.plot(ncfile.temperature, ncfile.acell[a, :]-shift, linestyle='dashed', **kwargs)
-
     def check_units(self, ncfile):
 
         if ncfile.yaxis_units.find('ang') != -1 or ncfile.yaxis_units.find('Ang') != -1:
@@ -116,8 +99,7 @@ class AcellPlot(TEplot):
         elif ncfile.yaxis_units.find('bohr') != -1 or ncfile.yaxis_units.find('Bohr') != -1:
             ncfile.yaxis = ncfile.yaxis * bohr_to_ang
         else:
-            raise Exception('''Units from EXPfile should contain either "ang/Ang" or "bohr/Bohr",
-                            but units found are {}'''.format(ncfile.yaxis_units))
+            raise Exception('Units from EXPfile should contain either "ang/Ang" or "bohr/Bohr"')
 
     @property
     def xlabel(self):
@@ -164,15 +146,11 @@ class AlphaPlot(TEplot):
                 else:
                     self.ax.plot(ncfile.temperature, ncfile.alpha[a, :]*1E6, **kwargs)
 
-            if i == 0:
-                self.set_yref(self.ax, ncfile.temperature, 0., 'dashed')
-
     def check_units(self, ncfile):
         if ncfile.yaxis_units.find('K') != -1:
             ncfile.yaxis = ncfile.yaxis*1E6
         else:
-            raise Exception('''Units from EXPfile should be "K^-1" and contain "K",
-                           but units found are {}'''.format(ncfile.yaxis_units))
+            raise Exception('Units from EXPfile should be "K^-1" and contain "K"')
 
     @property
     def xlabel(self):
@@ -199,10 +177,10 @@ class RoomAlphaPlot(AlphaPlot):
 
     def __init__(self, ax=None, field='room_alpha', **kwargs):
         self.field = field
-        super(RoomAlphaPlot, self).__init__(ax=ax, field=self.field)
+        super(RoomAlphaPlot, self).__init__(ax=ax)
 
     def plot_te(self, te_data, marker_list=None, color_list=None, **kwargs):
-        print('in te_plot', self.field)
+
         if self.field == 'room_alpha' or self.field == 'room_alpha_a':
             a = 0
         elif self.field == 'room_alpha_c':
@@ -220,15 +198,6 @@ class RoomAlphaPlot(AlphaPlot):
                     self.ax.plot(ncfile.temperature, ncfile.room_temp_alpha[a, :]*1E6, color=color_list[i], **kwargs)
                 else:
                     self.ax.plot(ncfile.temperature, ncfile.room_temp_alpha[a, :]*1E6, **kwargs)
-            if i == 0:
-                self.set_yref(self.ax, ncfile.temperature, 0., 'dashed')
-
-    def check_units(self, ncfile):
-        if ncfile.yaxis_units.find('K') != -1:
-            ncfile.yaxis = ncfile.yaxis*1E6
-        else:
-            raise Exception('''Units from EXPfile should be "K^-1" and contain "K",
-                           but units found are {}'''.format(ncfile.yaxis_units))
 
     @property
     def ylabel(self):
@@ -238,103 +207,6 @@ class RoomAlphaPlot(AlphaPlot):
             return r'$\alpha_c$ ($10^{-6}K^{-1}$), ref. 293K'
         else:
             return r'$\alpha$ ($10^{-6}K^{-1}$), ref. 293K'
-
-
-class GruneisenPlot(TEplot):
-    """
-    Plots temperature dependent lattice parameters
-    """
-
-    def __init__(self, ax=None, field='gruneisen', **kwargs):
-        self.field = field
-        super(GruneisenPlot, self).__init__(ax=ax)
-
-    def plot_te(self, te_data, marker_list=['o', 's', '^', 'd'], color_list=None, **kwargs):
-
-        if self.field == 'gruneisen' or self.field == 'gruneisen_a':
-            a = 0
-        elif self.field == 'gruneisen_c':
-            a = -1
-
-        for i, ncfile in enumerate(te_data):
-
-            if i == 0:
-                nmode = ncfile.nmode
-
-            for v in range(nmode):
-                if color_list is not None:
-                    self.ax.plot(ncfile.omega[:, v], ncfile.gruneisen[a, :, v], marker=marker_list[i],
-                                 color=color_list[i], linestyle='None')
-                else:
-                    self.ax.plot(ncfile.omega[:, v], ncfile.gruneisen[a, :, v], marker=marker_list[i], linestyle='None')
-
-            self.ax.grid(b=True, which='major')
-
-    @property
-    def xlabel(self):
-        return r'$\omega_{\mathbf{q}\nu}$ (meV)'
-
-    @property
-    def ylabel(self):
-        if self.field == 'gruneisen_a':
-            return r'Mode Gruneisen parameter $\gamma^a_{\mathbf{q}\nu}$'
-        elif self.field == 'gruneisen_c':
-            return r'Mode Gruneisen parameter $\gamma^c_{\mathbf{q}\nu}$'
-        else:
-            return r'Mode Gruneisen parameter $\gamma_{\mathbf{q}\nu}$'
-
-    def format_ticks(self):
-
-        self.ax.xaxis.set_major_formatter(FuncFormatter(self.float_formatter))
-        self.ax.yaxis.set_major_formatter(FuncFormatter(self.float_formatter))
-        plt.setp(self.ax.get_xticklabels(), fontsize=16, weight='bold')
-        plt.setp(self.ax.get_yticklabels(), fontsize=16, weight='bold')
-
-
-class SpecificHeatPlot(AlphaPlot):
-
-    def __init__(self, ax=None, field='specific_heat', **kwargs):
-        self.field = field
-        super(SpecificHeatPlot, self).__init__(ax=ax, field=self.field)
-
-    def plot_te(self, te_data, marker_list=None, color_list=None, **kwargs):
-
-        for i, ncfile in enumerate(te_data):
-            if marker_list:
-                if color_list:
-                    self.ax.plot(ncfile.temperature, ncfile.specific_heat*1E4, marker=marker_list[i],
-                                 color=color_list[i], **kwargs)
-                else:
-                    self.ax.plot(ncfile.temperature, ncfile.specific_heat*1E4, marker=marker_list[i], **kwargs)
-            else:
-                if color_list:
-                    self.ax.plot(ncfile.temperature, ncfile.specific_heat*1E4, color=color_list[i], **kwargs)
-                else:
-                    self.ax.plot(ncfile.temperature, ncfile.specific_heat*1E4, **kwargs)
-            if i == 0:
-                self.set_yref(self.ax, ncfile.temperature, 0., 'dashed')
-
-    def check_units(self, ncfile):
-        if ncfile.yaxis_units.find('K') != -1:
-            if ncfile.yaxis_units.find('J') != -1 and ncfile.yaxis_units.find('mol') != -1:
-                ncfile.yaxis = ncfile.yaxis*j_to_ev/avogadro*1E4
-            elif ncfile.yaxis_units.find('eV') == -1:
-                raise Exception(''' Implemented units for specific heat are eV K^-1
-                               and J mol^-1 K^-1, but units found are {}'''.format(ncfile.yaxis_units))
-        else:
-            raise Exception('''Units from EXPfile go as "K^-1" and contain "K",
-                           but units found are {}'''.format(ncfile.yaxis_units))
-
-    def format_ticks(self):
-
-        self.ax.xaxis.set_major_formatter(FuncFormatter(self.int_formatter))
-        self.ax.yaxis.set_major_formatter(FuncFormatter(self.float_formatter))
-        plt.setp(self.ax.get_xticklabels(), fontsize=16, weight='bold')
-        plt.setp(self.ax.get_yticklabels(), fontsize=16, weight='bold')
-
-    @property
-    def ylabel(self):
-        return r'C$_v$ (10$^{-4}$ eV K$^{-1}$)'
 
 
 def generate_plot(
@@ -347,21 +219,15 @@ def generate_plot(
         exp_marker_list=None,
         exp_color_list=None,
         label_size=16,
-        shift_acell=None,
 
         **kwargs):
 
     if field.find('acell') != -1:
-        myax = AcellPlot(ax=ax, field=field, shift_acell=shift_acell, **kwargs)
+
+        myax = AcellPlot(ax=ax, field=field, **kwargs)
 
     if field.find('alpha') != -1:
-        if field.find('room_alpha') != -1:
-            myax = RoomAlphaPlot(ax=ax, field=field, **kwargs)
-        else:
-            myax = AlphaPlot(ax=ax, field=field, **kwargs)
-
-    if field.find('specific_heat') != -1:
-        myax = SpecificHeatPlot(ax=ax, field=field, **kwargs)
+        myax = AlphaPlot(ax=ax, field=field, **kwargs)
 
     myax.plot_te(te_data, marker_list=marker_list, color_list=color_list, linewidth=1.5, **kwargs)
     if exp_data:
