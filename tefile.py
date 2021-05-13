26b1615e94e28879c1f15017afa505cc62ce0c4c
brousseauv <veronique.brousseauc@gmail.com>
2021-05-13 09:47:26 -0400

    fixed tefile for GruneisenPath

--- tefile.py
+++ tefile.py
@@ -25,15 +25,19 @@ class FreeEnergyFile(EpcFile):
             self.temp_units = dts.variables['temperature'].getncattr('units')
 
             self.acell = dts.variables['acell_from_gibbs'][:, :] * bohr_to_ang
-            self.free_energy = dts.variables['free_energy'][:,:] #* ha_to_ev  # in eV
-#            self.static_energy = dts.variables['static_energy'][:,:] #* ha_to_ev  # in eV
-#            self.phonon_free_energy = dts.variables['phonon_free_energy'][:,:] #* ha_to_ev  # in eV
-            self.volume = dts.variables['volume'][:,:]  # For now, leave in Bohr^3
+            self.free_energy = dts.variables['free_energy'][:, :]  # * ha_to_ev  # in eV
+            self.static_energy = dts.variables['static_energy'][:, :]  # * ha_to_ev  # in eV
+            self.phonon_free_energy = dts.variables['phonon_free_energy'][:, :]  # * ha_to_ev  # in eV
+            self.volume = dts.variables['volume'][:, :]  # For now, leave in Bohr^3
             self.equilibrium_volume = dts.variables['equilibrium_volume'][:]
+            self.equilibrium_volume[0] = self.equilibrium_volume[0] * bohr_to_ang**3
+            self.equilibrium_volume[1:] = self.equilibrium_volume[1:] * bohr_to_ang
             self.fit_parameters = dts.variables['fit_parameters'][:, :]
             self.fit_parameters_list = dts.variables['fit_parameters'].getncattr('units')
             self.fit_function = dts.variables['fit_parameters'].getncattr('description')
             self.eos_fit = dts.variables['volumic_fit_parameters'][:, :]  # For now, in Ha and Ha/bohr^3
+            self.bulk_modulus_from_eos = self.eos_fit[2, :] * habo3_to_gpa
+            self.specific_heat = dts.variables['specific_heat'][:] * ha_to_ev  # Should be in eV/K
 
             # For cubics, eos fit is FE fit
             if self.eos_fit.mask.all():
@@ -68,10 +72,8 @@ class GruneisenFile(EpcFile):
 #            self.acell = dts.variables['acell_from_gruneisen'][:, :] * bohr_to_ang
 #            self.acell_zpm = dts.variables['acell_from_gruneisen_plushalf'][:, :] * bohr_to_ang
             self.acell = dts.variables['acell_from_gruneisen_plushalf'][:, :] * bohr_to_ang
-            self.equilibrium_volume = dts.variables['equilibrium_volume'][:]
 
 #            self.free_energy = dts.variables['free_energy'][:] * ha_to_ev  # in eV
-#            self.volume = dts.variables['volume']  # For now, leave in Bohr^3
 #            self.fit_parameters = dts.variables['fit_parameters'][:, :]
 #            self.fit_parameters_list = dts.variables['fit_parameters'].getncattr('units')
 #            self.fit_function = dts.variables['fit_parameters'].getncattr('description')
@@ -85,6 +87,14 @@ class GruneisenFile(EpcFile):
             self.room_temp_alpha = dts.variables['room_temp_alpha'][:, :]
             self.specific_heat = dts.variables['specific_heat'][:] * ha_to_ev  # in eV/K
             self.bulk_modulus_from_elastic = dts.variables['bulk_modulus_habo3'][:] * habo3_to_gpa
+            self.volume = dts.variables['volume']  # For now, leave in Bohr^3
+            self.equilibrium_volume = dts.variables['equilibrium_volume'][:]
+            self.equilibrium_volume[0] = self.equilibrium_volume[0] * bohr_to_ang**3
+            self.equilibrium_volume[1:] = self.equilibrium_volume[1:] * bohr_to_ang
+            self.gruneisen = dts.variables['gruneisen_parameters'][:, :, :]
+            self.gruneisen_from_dynmat = dts.variables['gruneisen_parameters_from_dynmat'][:, :, :]
+            self.gruneisen_from_dynmat_rot = dts.variables['gruneisen_parameters_from_dynmat_rot'][:, :, :]
+            self.omega = dts.variables['omega_equilibrium'][:, :] * ha_to_ev * 1E3  # in meV
 
     @property
     def ntemp(self):
@@ -93,6 +103,41 @@ class GruneisenFile(EpcFile):
     @property
     def nacell(self):
         return self.acell.shape[0]
+
+
+class GruneisenPathFile(EpcFile):
+
+    def read_nc(self, fname=None):
+
+        # Open the .nc file and read it
+        fname = fname if fname else self.fname
+
+        super(GruneisenPathFile, self).read_nc(fname)
+
+        with nc.Dataset(fname, 'r') as dts:
+
+            self.bulk_modulus_from_elastic = dts.variables['bulk_modulus_habo3'][:] * habo3_to_gpa
+            self.volume = dts.variables['volume']  # For now, leave in Bohr^3
+            self.equilibrium_volume = dts.variables['equilibrium_volume'][:]
+            self.equilibrium_volume[0] = self.equilibrium_volume[0] * bohr_to_ang**3
+            self.equilibrium_volume[1:] = self.equilibrium_volume[1:] * bohr_to_ang
+            self.gruneisen_from_dynmat = dts.variables['gruneisen_parameters_from_dynmat'][:, :, :]
+            # self.gruneisen_from_dynmat_rot = dts.variables['gruneisen_parameters_from_dynmat_rot'][:, :, :]
+            self.omega = dts.variables['omega_equilibrium'][:, :] * ha_to_ev * 1E3  # in meV
+            # self.omega_rot = dts.variables['omega_equilibrium_rot'][:, :] * ha_to_ev * 1E3  # in meV
+            self.qred = dts.variables['reduced_coordinates_of_qpoints'][:, :]
+
+    @property
+    def nacell(self):
+        return self.gruneisen_from_dynmat.shape[0]
+
+    @property
+    def nqpt(self):
+        return self.gruneisen_from_dynmat.shape[1]
+
+    @property
+    def nmode(self):
+        return self.gruneisen_from_dynmat.shape[2]
 
 
 def set_file_class(fname):
